79bd1122fef3190a1c030c685b294bf8088f32b6
elbichorecuperar-lang <elbichorecuperar@gmail.com>
2026-08-23 17:56:14 -0400

    Update main.cpp

--- minimal/src/main.cpp
+++ minimal/src/main.cpp
@@ -1,3 +1,14 @@
-#include <Geode/Geode.hpp>
+#include <Geode/Geode.include>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
+
+class $modify(MyPlayLayer, PlayLayer) {
+    void handleButton(bool down, int button, bool isPlayer1) {
+        PlayLayer::handleButton(down, button, isPlayer1);
+
+        if (down && m_isPracticeMode) {
+            this->addCheckpoint();
+        }
+    }
+};
